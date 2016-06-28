c05d951f465374b70831d4cc1b009f2c54521524
Kyle Paulsen <kyle.paulsen@autodesk.com>
2016-06-28 16:24:53 -0700

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+
+var MAIN_PATH = './src/main.js';
+var BUILD_PATH = './build.js';
+var WATCH_PATH = './src/*.js';
+
+gulp.task('dev', function() {
+    gulp.src(MAIN_PATH)
+        .pipe(browserify({
+            debug: true
+        }))
+        .pipe(rename(BUILD_PATH))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('prod', function() {
+    gulp.src(MAIN_PATH)
+        .pipe(browserify({
+            debug: false
+        }))
+        .pipe(uglify({
+            mangle: true
+        }))
+        .pipe(rename(BUILD_PATH))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch(WATCH_PATH, ['dev']);
+});
+
+gulp.task('default', ['dev']);
